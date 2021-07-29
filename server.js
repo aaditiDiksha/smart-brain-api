961fefb976501f48e62939ebc48850514b344f18
aaditidiksha <aditi.d1231@gmail.com>
2021-07-29 19:37:52 +0530

    fix db connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const auth = require("./controllers/authorization");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
