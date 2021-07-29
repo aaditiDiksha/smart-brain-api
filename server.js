971c1d83a00a9c4fce71603c5dced3f7f5985eed
aaditidiksha <aditi.d1231@gmail.com>
2021-07-29 19:29:25 +0530

    updating database

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const auth = require("./controllers/authorization");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.POSTGRES_HOST,
+    host: "postgresql-vertical-73693",
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
