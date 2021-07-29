389202e83e3ff9ff4ce6c9b2369d5918f5a66b79
aaditidiksha <aditi.d1231@gmail.com>
2021-07-29 17:30:04 +0530

    changes in scripts and port to env.port

--- server.js
+++ server.js
@@ -1,56 +1,58 @@
-    /*"start": "node server.js" */
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-const { response } = require('express');
+/*"start": "node server.js" */
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const { response } = require("express");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-const auth = require('./controllers/authorization')
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const auth = require("./controllers/authorization");
 
- const db = knex({
-   client: "pg",
-   connection: {
-     host: process.env.POSTGRES_HOST,
-     user: process.env.POSTGRES_USER,
-     password: process.env.POSTGRES_PASSWORD,
-     database: process.env.POSTGRES_DB,
-   },
- });
-
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
 
 const app = express();
 
-
-app.use(cors()); 
-app.use(express.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req,res) => { res.send('working') })
-app.post('/signin', (req,res)=> signin.signinAuthentication(req,res,db, bcrypt))
-app.post('/register', (req, res) => { register.generateAuthToken(req, res, db, bcrypt) })
+app.get("/", (req, res) => {
+  res.send("working");
+});
+app.post("/signin", (req, res) =>
+  signin.signinAuthentication(req, res, db, bcrypt)
+);
+app.post("/register", (req, res) => {
+  register.generateAuthToken(req, res, db, bcrypt);
+});
 // app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
-app.get("/profile/:id",auth.requireAuth, (req, res) => {
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
   profile.handleProfileGet(req, res, db);
 });
 
-app.put("/image",  (req, res) => {
+app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
-app.post("/imageurl",  (req, res) => {
+app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-
-
-app.listen(3000, () =>{
-    console.log("app is running on port 3000");
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
 
 /*
 / --> res = this is working
